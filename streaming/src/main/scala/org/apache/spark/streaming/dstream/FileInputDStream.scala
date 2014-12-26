9c5329bbb7cad261996a79d15178882e459513e1
wangxiaojing <u9jing@gmail.com>
2014-12-26 01:59:47 -0800

    change style

--- streaming/src/main/scala/org/apache/spark/streaming/dstream/FileInputDStream.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/dstream/FileInputDStream.scala
@@ -33,9 +33,10 @@ import org.apache.spark.util.{TimeStampedHashMap, Utils}
  * This class represents an input stream that monitors a Hadoop-compatible filesystem for new
  * files and creates a stream out of them. The way it works as follows.
  *
- * At each batch interval, the file system is queried for files in the given directory and
- * detected new files are selected for that batch. Use `depth` to control the searching
- * depth of directories. In this case "new" means files that
+ * At each batch interval, Use `depth` to control the searching depth of directories ,
+ * if the `depth` is default 1, the file system is queried for files in the given directory,
+ * if the `depth` is greater than 1 ,it is  queried for files in nested directory,
+ * and detected new files are selected for that batch. In this case "new" means files that
  * became visible to readers during that time period. Some extra care is needed to deal
  * with the fact that files may become visible after they are created. For this purpose, this
  * class remembers the information about the files selected in past batches for
@@ -66,7 +67,6 @@ import org.apache.spark.util.{TimeStampedHashMap, Utils}
  *   selected as the mod time will be less than the ignore threshold when it becomes visible.
  * - Once a file is visible, the mod time cannot change. If it does due to appends, then the
  *   processing semantics are undefined.
- *
  */
 private[streaming]
 class FileInputDStream[K: ClassTag, V: ClassTag, F <: NewInputFormat[K,V] : ClassTag](
@@ -150,7 +150,7 @@ class FileInputDStream[K: ClassTag, V: ClassTag, F <: NewInputFormat[K,V] : Clas
   }
 
   /**
-   * Find new files for the batch of `currentTime` in nested directories. 
+   * Find new files for the batch of `currentTime` in nested directories.
    * This is done by first calculating the
    * ignore threshold for file mod times, and then getting a list of files filtered based on
    * the current batch time and the ignore threshold. The ignore threshold is the max of
@@ -376,7 +376,7 @@ object FileInputDStream {
   def defaultFilter(path: Path): Boolean = !path.getName().startsWith(".")
 
   /**
-   * Calculate the number of last batches to remember, such that all the files selected
+   * Calculate the number of last batches to remember, such that all the files selected in
    * at least last MIN_REMEMBER_DURATION duration can be remembered.
    */
   def calculateNumBatchesToRemember(batchDuration: Duration): Int = {
